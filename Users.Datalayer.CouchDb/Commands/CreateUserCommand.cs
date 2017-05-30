30c0ef9af414412bf6fcb6ddf4f8d5e2e91f4548
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-05-30 14:01:45 +0300

    Реализация работы с пользователями. Админская роль

---
+++ Users.Datalayer.CouchDb/Commands/CreateUserCommand.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Cmas.BusinessLayers.Users.CommandsContexts;
+using Cmas.DataLayers.CouchDb.Users.Dtos;
+using Cmas.DataLayers.Infrastructure;
+using Cmas.Infrastructure.Domain.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace Cmas.DataLayers.CouchDb.Users.Commands
+{
+    public class CreateUserCommand : ICommand<CreateUserCommandContext>
+    {
+        private readonly IMapper _autoMapper;
+        private readonly CouchWrapper _couchWrapper;
+
+        public CreateUserCommand(IServiceProvider serviceProvider)
+        {
+            _autoMapper = (IMapper) serviceProvider.GetService(typeof(IMapper));
+
+            _couchWrapper = new CouchWrapper(serviceProvider, DbConsts.ServiceName);
+        }
+
+        public async Task<CreateUserCommandContext> Execute(CreateUserCommandContext commandContext)
+        {
+            var doc = _autoMapper.Map<UserDto>(commandContext.User);
+
+            var result = await _couchWrapper.GetResponseAsync(async (client) =>
+            {
+                return await client.Entities.PostAsync(doc);
+            });
+
+            commandContext.Id = result.Id;
+
+            return commandContext;
+        }
+    }
+}
---
+++ Users.Datalayer.CouchDb/Commands/DeleteUserCommand.cs
@@ -0,0 +1,30 @@
+﻿using Cmas.BusinessLayers.Users.CommandsContexts;
+using Cmas.DataLayers.Infrastructure;
+using Cmas.Infrastructure.Domain.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace Cmas.DataLayers.CouchDb.Users.Commands
+{
+    public class DeleteUserCommand : ICommand<DeleteUserCommandContext>
+    {
+        private readonly CouchWrapper _couchWrapper;
+
+        public DeleteUserCommand(IServiceProvider serviceProvider)
+        {
+            _couchWrapper = new CouchWrapper(serviceProvider, DbConsts.ServiceName);
+        }
+
+        public async Task<DeleteUserCommandContext> Execute(DeleteUserCommandContext commandContext)
+        {
+            var header = await _couchWrapper.GetHeaderAsync(commandContext.Id);
+
+            var result = await _couchWrapper.GetResponseAsync(async (client) =>
+            {
+                return await client.Documents.DeleteAsync(commandContext.Id, header.Rev);
+            });
+
+            return commandContext;
+        }
+    }
+}
